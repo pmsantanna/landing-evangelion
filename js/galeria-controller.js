e9a91f66bf504e2a42fd754ed2646f0a2c040309
pedro.santanna <pedro1997santanna@gmail.com>
2023-03-28 10:44:39 -0300

    add working gallery controller

--- js/galeria-controller.js
+++ js/galeria-controller.js
@@ -1,21 +1,42 @@
 export default function () {}
 
 const galleryControllers = document.querySelectorAll(".gallery-item");
-const galleryImgSrc = document.getElementById("galeria-img");
-const galleryImgElement = document.querySelector(".conteudo-galeria img");
-
-console.log(galleryImgElement);
+const galleryDiv = document.querySelector(".conteudo-galeria");
+const galleryChildren = [...galleryDiv.children];
+const content = [...document.querySelectorAll(".conteudo-item")];
+const cards = document.querySelector(".cards");
 
 galleryControllers.forEach((item) => {
   item.addEventListener("click", changeImage);
 });
 
-function changeImage(event) {
-  const elementId = event.target.id;
-  galleryImgSrc.innerHTML = "teste";
-  galleryImgSrc.src = "./img/fichas/" + event.target.id + ".png";
-  if (elementId === "01") {
-  } else if (elementId === "02") {
-  } else if (elementId === "03") {
+cards.addEventListener("click", function (event) {
+  if (event.target.tagName === "DIV") {
+    changeImage(event);
   }
+});
+
+function changeImage(event) {
+  let elementId;
+  if (event.target.tagName === "DIV") {
+    elementId = event.target.classList[1];
+    elementId = elementId.charAt(elementId.length - 1);
+    galleryControllers.forEach((controller, index) => {
+      controller.checked = false;
+    });
+    galleryControllers[elementId - 1].checked = true;
+  }
+  if (event.target.tagName === "INPUT") {
+    elementId = event.target.id;
+  }
+  galleryChildren.forEach((element) => {
+    element.classList.remove("active");
+  });
+  content.forEach((element, index) => {
+    element.classList.remove("active");
+    if (index === elementId - 1) {
+      element.classList.toggle("active");
+    }
+  });
+  galleryChildren[elementId - 1].classList.toggle("active");
 }
